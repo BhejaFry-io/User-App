48453a33571fef687b98b1f0459dba44f6318095
Swayamjimmy <swayamwr@gmail.com>
2026-04-23 20:04:03 +0530

    minor fixes

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://bhejafry.fun/api';
+const BASE_URL = 'https://api.bhejafry.fun/api';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
